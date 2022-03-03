fe07af201d868a9024f228531156f66c06c6080d
alstroie <alstroie@pentalog.com>
2022-03-03 15:44:00 +0200

    nada

---
+++ curs-3/src/main/java/JavaTeacher.java
@@ -0,0 +1,29 @@
+public class JavaTeacher implements ITeacher{
+
+    private WisdomWordsService wisdomService;
+    private HomeworkService homeworkService;
+
+    // constructor unic care primeste ca parametru un obiect de tipul WisdomWordsService
+    public JavaTeacher(WisdomWordsService wisdomService, HomeworkService  homeworkService) {
+        this.wisdomService = wisdomService;
+        this.homeworkService = homeworkService;
+    }
+
+
+    @Override
+    public String getHomeWork() {
+        return "Create 100 classes";
+    }
+
+    @Override
+    public String getHomework() {
+        return homeworkService.getMessage();
+    }
+
+    @Override
+    public String getWisdom() {
+        return wisdomService.getMessage();
+    }
+
+
+}
---
+++ curs-3/src/main/java/MyFirstSpringApp.java
@@ -0,0 +1,50 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class MyFirstSpringApp{
+
+    public static void main(String[] args) {
+
+        // create the object
+//        TeacherFactory teacherFactory = new TeacherFactory();
+//        String teacherType = "";
+//
+//        try (InputStream input = new FileInputStream("config.properties")) {
+//
+//            Properties prop = new Properties();
+//
+//            // load a properties file
+//            prop.load(input);
+//            teacherType = prop.getProperty("teacherType");
+//
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//        ITeacher theTeacher = teacherFactory.getTeacher(teacherType);
+//
+//        // use the object
+//        System.out.println(theTeacher.getHomework());
+//        MyContainer myContainer = new MyContainer();
+//        ITeacher teacher = myContainer.getTeacher();
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        ITeacher theTeacher = context.getBean("myTeacher", ITeacher.class);
+
+        // call methods on the bean
+        System.out.println(theTeacher.getWisdom());
+        System.out.println(theTeacher.getHomeWork());
+        System.out.println(theTeacher.getHomework());
+
+        // close the context
+        context.close();
+
+    }
+
+}
