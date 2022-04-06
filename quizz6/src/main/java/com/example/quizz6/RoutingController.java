d5fe9d9ea01b53d301055e2cc38aaa8cfac902a4
alstroie <alstroie@pentalog.com>
2022-04-06 19:06:43 +0300

    quizz6

--- quizz6/src/main/java/com/example/quizz6/RoutingController.java
+++ quizz6/src/main/java/com/example/quizz6/RoutingController.java
@@ -6,8 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RoutingController {
 
     @Autowired
